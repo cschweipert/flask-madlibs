0dcf3bf0f6d1bede49f6aceb77c118c21c3d5c9c
Christina Schweipert <c.schweipert@gmail.com>
2021-03-03 18:27:16 -0700

    creates story and diplays it

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template, request
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
-debug = DebugToolbarExtension(app)
+# debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def ask_questions():
+    """Generate and show form to ask words."""
+
+    prompts = silly_story.prompts
+
+    return render_template("questions.html", prompts=prompts)
+
+
+@app.route("/story")
+def show_results():
+    """Show story result."""
+
+    text = silly_story.generate(request.args)
+
+    return render_template("story.html", text=text)
